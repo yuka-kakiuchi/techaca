408394e2fe62b443091745ef1fb3e7b6967d9157
yuka kakiuchi <g1633007k@sonoda-u.ac.jp>
2016-05-25 18:58:48 +0900

    １回目

--- practice.php
+++ practice.php
@@ -18,50 +18,50 @@
     <input type="text" name="no2" size="10">        <!--２つめの箱-->
     ＝
 
-<!--答えの部分-->
-<?php
+    <!--答えの部分-->
+    <?php
     //number1とnumber2に値がセットされる時のみ処理をする
     if(isset($_POST['no1']) && isset($_POST['no2'])&&isset($_POST['select'])) {
         $answer="";
 
-          //数値かどうかの判断
-          if (is_numeric($_POST['no1']) && is_numeric($_POST['no2'])) {
-                     //+の場合
-                     if ($_POST['select'] == 'plus') {
-                         $answer = $_POST['no1'] + $_POST['no2'];
-                     }
-                     //-の場合
-                     else if ($_POST['select'] == 'minus') {
-                         $answer = $_POST['no1'] - $_POST['no2'];
-                     }
-                     //×の場合
-                     else if ($_POST['select'] == 'multiply') {
-                         $answer = $_POST['no1'] * $_POST['no2'];
-                     }
-                     //÷の場合
-                     else if ($_POST['select'] == 'division') {
-                           //0で割る場合
-                             if ($_POST['no2'] == 0){
-                                 print "0で割れません";
-                            }else{
-                                 $answer = $_POST['no1'] / $_POST['no2'];
-                                  }
-                             }
-             }else {
-             print "数字を入れてください";
-         }echo "$answer";
-     }
+        //数値かどうかの判断
+        if (is_numeric($_POST['no1']) && is_numeric($_POST['no2'])) {
+            //+の場合
+            if ($_POST['select'] == 'plus') {
+                $answer = $_POST['no1'] + $_POST['no2'];
+            }
+            //-の場合
+            else if ($_POST['select'] == 'minus') {
+                $answer = $_POST['no1'] - $_POST['no2'];
+            }
+            //×の場合
+            else if ($_POST['select'] == 'multiply') {
+                $answer = $_POST['no1'] * $_POST['no2'];
+            }
+            //÷の場合
+            else if ($_POST['select'] == 'division') {
+                //0で割る場合
+                if ($_POST['no2'] == 0){
+                    print "0で割れません";
+                }else{
+                    $answer = $_POST['no1'] / $_POST['no2'];
+                }
+            }
+        }else {
+            print "数字を入れてください";
+        }echo "$answer";
+    }
     else {
         print "計算できません";
     }
     ?>
-<!--答えの部分終わり-->
+    <!--答えの部分終わり-->
 
 
- <br/>
+    <br/>
     <!--機能BOX-->
-　　<input type="submit" value="計算"/>
-　　<input type="submit" value="クリア">
+    　　<input type="submit" value="計算"/>
+    　　<input type="submit" value="クリア">
 
 </form>
 </body>
