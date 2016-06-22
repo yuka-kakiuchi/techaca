aed3eff182b97c4a3095cc1eebc3e72214f38ce2
yuka kakiuchi <g1633007k@sonoda-u.ac.jp>
2016-06-22 18:51:54 +0900

    初回です。

---
+++ board1/db.php
@@ -0,0 +1,17 @@
+<?php
+     function getDb(){
+           //DBの接続情報
+           $dsn = 'mysql:dbname=post;host=localhost';
+           $user = 'techaca';
+           $password = 'kaki.tech';
+         
+            try {
+               $db = new PDO($dsn, $user, $password);    //データベース接続
+               $db->exec('SET NAMES utf8');           //print '接続成功';
+            } catch (PDOException $e) {                   //エラー対応処理
+               die("接続エラー：{$e->getMessage()}");
+            }
+        return $db;
+     }
+
+?>
---
+++ board1/escape.php
@@ -0,0 +1,6 @@
+<!--HTMLエスケープ-->
+<?php
+function e($str,$charset='UTF-8'){
+    print htmlspecialchars($str,ENT_QUOTES,$charset);
+}
+?>
